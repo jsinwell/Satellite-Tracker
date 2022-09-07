270ec2b2698471dcd4cc3eb6674d705756a53231
Jon Sinwell <jsinwell@usc.edu>
2022-09-07 05:34:16 -0700

    First iteration of About page

--- src/js/main.js
+++ src/js/main.js
@@ -56,6 +56,7 @@ const viewer = new Cesium.Viewer('cesiumContainer', {
     }
 
     catch(err) {
+      console.log("Bad TLE data detected.");
       continue;
     }
 
@@ -135,11 +136,7 @@ const viewer = new Cesium.Viewer('cesiumContainer', {
           pixelSize: 4, color: Cesium.Color.GREEN}
       });
     }
-
-    if(satellitePoint[i].name == "ISS (ZARYA)") {
-      satellitePoint[i].description = 'The International Space Station is a large spacecraft in orbit around Earth. It serves as a home where crews of astronauts and cosmonauts live. The space station is also a unique science laboratory. Several nations worked together to build and use the space station. The space station is made of parts that were assembled in space by astronauts. It orbits Earth at an average altitude of approximately 250 miles. It travels at 17,500 mph. This means it orbits Earth every 90 minutes. NASA is using the space station to learn more about living and working in space. These lessons will make it possible to send humans farther into space than ever before.';
-    }
-
+    
     i++;
 
   }
